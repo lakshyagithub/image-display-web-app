6755ecde94b335da9673c67e35147e2af3b6e495
LAKSHYA SINGH CHAUHAN <85747258+lakshyagithub@users.noreply.github.com>
2022-02-15 09:25:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 function Showimage() {
-    data_image = document.getElementById("file");
+    data_image = document.getElementById("file").value;
     document.getElementById("image").innerHTML='<img src="'+data_image+'"/>'
 }
